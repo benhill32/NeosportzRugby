03bf7360d91a227384795631bebc6abedf8b7b6e
benhill32 <ben@neocom.co.nz>
2015-06-24 12:00:25 +1200

    1
    1

--- www/js/schedule.js
+++ www/js/schedule.js
@@ -498,7 +498,7 @@ var name = "game" + ID;
                 alert(error);
             }else{
               alert('ok' + res.filePath); //should be path/to/myScreenshot.jpg
-                $('#thepicture').attr("src", res.filepath);
+                $('#thepicture').attr("src", res.URI);
                 $('#basicModalimagecrop').modal('show');
 
             }
