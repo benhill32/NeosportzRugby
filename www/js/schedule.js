96a105f32d0dcabec20c19962d36d6d19f18c6af
benhill32 <ben@neocom.co.nz>
2015-06-25 14:21:40 +1200

    1
    1

--- www/js/schedule.js
+++ www/js/schedule.js
@@ -518,7 +518,8 @@ function loadsocialIOS(ID){
 
     var message = mess[1] + "" + ampm + '  ' + day + '/' +  month + '/' + year + "" + mess[2] + "" + mess[3];
     //alert(message);
-        window.plugins.socialsharing.share(message);
+        //window.plugins.socialsharing.share(message);
+    window.plugins.socialsharing.shareViaFacebook(message, null /* img */, null /* url */, function() {console.log('share ok')}, function(errormsg){alert(errormsg)});
 }
 
 
