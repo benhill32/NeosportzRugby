aca907bbde0883ec70cf4eb186ef3c371086e2a1
benhill32 <ben@neocom.co.nz>
2015-11-19 11:33:44 +1300

    1
    1

--- www/js/functions.js
+++ www/js/functions.js
@@ -627,10 +627,7 @@ function syncmaintables(obj,year){
     var datenow = new Date();
     functionyear = year;
 
-    db.transaction(function (tx) {
-        tx.executeSql('Delete from MobileApp_Schedule_Menu');
-        // console.log('Delete MobileApp_Results where ID =' + obj.ID);
-    });
+   
 
     $.each(obj.App_Games, function (idx, obj) {
 
