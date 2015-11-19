40ff6541dbfa7a8caad76c6347f57bc8c9aadef3
benhill32 <ben@neocom.co.nz>
2015-11-20 08:18:22 +1300

    1
    1

--- www/js/clubs.js
+++ www/js/clubs.js
@@ -3,28 +3,63 @@ var IDhist = 0;
 var IDcon = 0;
 var ID = 0;
 var FirstID = 0;
+var LastID = 0;
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
  //   db = window.openDatabase("Neosportz_Football", "1.1", "Neosportz_Football", 200000);
     console.log("LOCALDB - Database ready");
-
-
     db.transaction(getfirstclub, errorCBfunc, successCBfunc);
-
-   //
 }
-
 
 function getfirstclub(tx) {
 
-    var sql = "select ID,_id ,name,UpdateDateUTC ,Base64,replace(History, '###$$###', '<br>') as History,replace(Contacts, '###$$###', '<br>') as Contacts,UpdateSecondsUTC,Color from MobileApp_clubs ORDER BY ID ASC LIMIT 1";
-    //alert(sql);
+    var sql = "select ID from MobileApp_clubs WHERE DeletedateUTC = 'null' ORDER BY ID ASC LIMIT 1";
     tx.executeSql(sql, [], getfirstclub_success);
 }
 
 
 function getfirstclub_success(tx, results) {
+
+    var len = results.rows.length;
+    var menu = results.rows.item(0);
+
+
+    FirstID = menu.ID;
+
+    db.transaction(getlastclub, errorCBfunc, successCBfunc);
+
+}
+
+function getlastclub(tx) {
+
+    var sql = "select ID from MobileApp_clubs WHERE DeletedateUTC = 'null' ORDER BY ID DESC LIMIT 1";
+    //alert(sql);
+    tx.executeSql(sql, [], getlastclub_success);
+}
+
+
+function getlastclub_success(tx, results) {
+
+    var len = results.rows.length;
+    var menu = results.rows.item(0);
+
+
+    LastID = menu.ID;
+
+    db.transaction(getclub, errorCBfunc, successCBfunc);
+
+}
+
+function getclub(tx) {
+
+    var sql = "select ID,_id ,name,UpdateDateUTC ,Base64,replace(History, '###$$###', '<br>') as History,replace(Contacts, '###$$###', '<br>') as Contacts,UpdateSecondsUTC,Color from MobileApp_clubs ORDER BY ID ASC LIMIT 1";
+    //alert(sql);
+    tx.executeSql(sql, [], getclub_success);
+}
+
+
+function getclub_success(tx, results) {
 
     var len = results.rows.length;
     var menu = results.rows.item(0);
@@ -41,12 +76,6 @@ function getfirstclub_success(tx, results) {
     $('#divTeams').append();
     $('#divPlayers').append();
     ID = menu.ID;
-    FirstID = menu.ID;
-
-    if(FirstID == menu.ID){
-
-        $("spanleft").hide();
-    }
 }
 
 
@@ -93,26 +122,32 @@ function getMenu_success(tx, results) {
 
     if(FirstID == menu.ID){
 
-        $("spanleft").hide();
+        $("#spanleft").hide();
     }else{
 
-        $("spanleft").show();
+        $("#spanleft").show();
     }
 }
 
 
 function getpervoiusclub(){
 
-
-    db.transaction(getdataminus, errorCBfunc, successCBfunc);
-
+    if(FirstID == ID) {
+        db.transaction(getdataplus, errorCBfunc, successCBfunc);
+    }else{
+        db.transaction(getdataminus, errorCBfunc, successCBfunc);
+    }
 
 }
 function getnextclub(){
 
 
+    if(LastID == ID) {
+        db.transaction(getdataminus, errorCBfunc, successCBfunc);
+    }else{
+        db.transaction(getdataplus, errorCBfunc, successCBfunc);
+    }
 
-    db.transaction(getdataplus, errorCBfunc, successCBfunc);
 
 
 }
