d9a28a66b037325774a0fa4b6981f77020561af1
benhill32 <ben@neocom.co.nz>
2015-12-07 13:40:12 +1300

    1
    1

--- www/js/news.js
+++ www/js/news.js
@@ -53,6 +53,8 @@ function numbersponsers_success(tx, results) {
     var len = results.rows.length;
     var menu2 = results.rows.item(0);
     nospor = menu2.Countall;
+    alert(nospor);
+
 
     var random = Math.floor((Math.random() * nospor) + 1);
 
