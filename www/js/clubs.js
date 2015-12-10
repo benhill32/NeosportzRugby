eb004b73e5f7f7472cc34c6393f6c26c2737a683
benhill32 <ben@neocom.co.nz>
2015-12-11 09:28:54 +1300

    1
    1

--- www/js/clubs.js
+++ www/js/clubs.js
@@ -95,7 +95,31 @@ function getclub_success(tx, results) {
     clubname = menu.ID;
     clubnewfeed = menu.Newfeed
     $('#divhistory').append(menu.History);
-    $('#divContacts').append(menu.Contacts);
+
+
+    // parse a string for numbers
+    var numbers = new PhoneNumberParser();
+
+    var contacts = menu.Contacts
+
+    numbers.parse(contacts);
+    var pnumbers = [];
+    for (i = 0; i < numbers.items.length; i++) {
+
+
+
+        if(jQuery.inArray( numbers.items[i], pnumbers ) == "-1") {
+            contacts = contacts.replace(new RegExp(numbers.items[i],'g'),"<a href='tel:" + numbers.items[i] + "'>" + numbers.items[i] + "</a>");
+
+            pnumbers.push(numbers.items[i]);
+        }
+
+    }
+
+
+    $('#divContacts').append(contacts);
+
+
 
 if(menu.ID == window.localStorage.getItem("teamfollow")){
 
@@ -301,3 +325,55 @@ function getteamplayer_success(tx, results) {
 
 
 }
+
+
+var PhoneNumberParser = function() {
+
+    var minimum = 9;            // typical minimum phone number length
+    this.items = [];
+
+    var public = PhoneNumberParser.prototype;
+    public.parse = function(str) {
+        var items = this.items = [];
+
+        var i = 0, n = '', min = minimum;
+
+        while(i < str.length) {
+            switch(str[i]) {
+                case '+':                                   // start of international number
+                    if (n.length >= min) items.push(n);
+                    n = str[i];
+                    min = minimum + 2;                      // at least 2 more chars in number
+                    break;
+                case '-': case '.': case '(': case ')':     // ignore punctuation
+                break;
+                case ' ':
+                    if (n.length >= min) {              // space after consuming enough digits is end of number
+                        items.push(n);
+                        n = '';
+                    }
+                    break;
+                default:
+                    if (str[i].match(/[0-9]/)) {            // add digit to number
+                        n += str[i];
+                        if (n.length == 1 && n != '0') {
+                            min = 3;                        // local number (extension possibly)
+                        }
+                    } else {
+                        if (n.length >= min) {
+                            items.push(n);                  // else end of number
+                        }
+                        n = '';
+                    }
+                    break;
+            }
+            i++;
+        }
+
+        if (n.length >= min) {              // EOF
+            items.push(n);
+        }
+    }
+
+
+};
