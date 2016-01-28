46873bd3fd24c68f6731fff9747cdabdaf2031bd
benhill32 <ben@neocom.co.nz>
2016-01-29 11:15:17 +1300

    1
    1

--- www/js/dbconnection.js
+++ www/js/dbconnection.js
@@ -10,11 +10,11 @@ function onDeviceReadydbconn() {
       //  alert(deviceplatformdb);
        // db = window.sqlitePlugin.openDatabase("../Library/Caches/NeosportzFootball", "1.1", "Neosportz_Football", 200000);
 
-        var db = window.sqlitePlugin.openDatabase({name: "Neosportz_Football", location: 1}, successCBfunc, errorCBfunc);
+         db = window.sqlitePlugin.openDatabase({name: "Neosportz_Football", location: 1}, successCBfunc, errorCBfunc);
     }else if(deviceplatformdb == "Android"){
        // alert(deviceplatformdb);
-       // db = window.sqlitePlugin.openDatabase("Neosportz_Football", "1.1", "Neosportz_Football", 200000);
-        var db = window.sqlitePlugin.openDatabase({name: "Neosportz_Football", androidDatabaseImplementation: 2, androidLockWorkaround: 1}, successCBfunc, errorCBfunc);
+        //db = window.sqlitePlugin.openDatabase("Neosportz_Football", "1.1", "Neosportz_Football", 200000);
+         db = window.sqlitePlugin.openDatabase({name: "Neosportz_Football", androidDatabaseImplementation: 2, androidLockWorkaround: 1}, successCBfunc, errorCBfunc);
 
     }
 }
